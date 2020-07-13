680f8ee6e89682be7ed97fde3a0c525a37ce20bc
“[Nicol <[nicol.saha@gmail.com]>
2020-07-13 16:57:09 +0200

    Finish maths-02/6

--- 02-maths/06-fizzbuzz/script.js
+++ 02-maths/06-fizzbuzz/script.js
@@ -3,14 +3,29 @@
  * /02-maths/06-fizzbuzz/script.js - 2.6: fizzbuzz
  *
  * coded by leny@BeCode
- * started at 26/10/2018
+ * ied at 26/10/2018
  */
 
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
+  // your code here
 
-    // your code here
-
+  n = 100;
+  for (i = 1; i <= n; i + 1) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log(i + " fizzbuzz");
+      i++;
+    } else if (i % 3 === 0) {
+      console.log(i + " fizz");
+      i++;
+    } else if (i % 5 === 0) {
+      console.log(i + " buzz");
+      i++;
+    } else {
+      console.log(i);
+      i++;
+    }
+  }
 })();
