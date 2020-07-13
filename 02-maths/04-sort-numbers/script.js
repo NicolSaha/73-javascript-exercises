ae549eb72f3095f48c884c573fa2a0810a9bd459
“[Nicol <[nicol.saha@gmail.com]>
2020-07-13 15:59:33 +0200

    Finish maths-02/4

--- 02-maths/04-sort-numbers/script.js
+++ 02-maths/04-sort-numbers/script.js
@@ -9,14 +9,17 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
+  // to get the value of an input: document.getElementById("element-id").value
 
-    // to get the value of an input: document.getElementById("element-id").value
+  document.getElementById("run").addEventListener("click", function () {
+    // your code here
 
-    document.getElementById("run").addEventListener("click", function() {
-
-        // your code here
-
+    let input = document.getElementById("numbers");
+    let numberSet = input.value.split(",");
+    let sort = numberSet.sort(function (a, b) {
+      return a - b;
     });
-
+    input.value = sort;
+  });
 })();
