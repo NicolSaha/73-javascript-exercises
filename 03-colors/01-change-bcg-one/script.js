8a8e8443514c8f8c5a056d5cf44768b0bcfda31e
“[Nicol <[nicol.saha@gmail.com]>
2020-07-14 10:52:52 +0200

    Finish colors-03/1

--- 03-colors/01-change-bcg-one/script.js
+++ 03-colors/01-change-bcg-one/script.js
@@ -9,8 +9,51 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
+  // your code here
+  var isColored = false;
 
-    // your code here
+  var buttonRed = document.getElementById("red");
+  buttonRed.addEventListener("click", function () {
+    if (isColored) {
+      document.body.style.background = "#16232E";
+      isColored = false;
+    } else {
+      document.body.style.background = "red";
+      isColored = true;
+    }
+  });
 
+  var buttonGreen = document.getElementById("green");
+  buttonGreen.addEventListener("click", function () {
+    if (isColored) {
+      document.body.style.background = "#16232E";
+      isColored = false;
+    } else {
+      document.body.style.background = "green";
+      isColored = true;
+    }
+  });
+
+  var buttonYellow = document.getElementById("yellow");
+  buttonYellow.addEventListener("click", function () {
+    if (isColored) {
+      document.body.style.background = "#16232E";
+      isColored = false;
+    } else {
+      document.body.style.background = "yellow";
+      isColored = true;
+    }
+  });
+
+  var buttonBlue = document.getElementById("blue");
+  buttonBlue.addEventListener("click", function () {
+    if (isColored) {
+      document.body.style.background = "#16232E";
+      isColored = false;
+    } else {
+      document.body.style.background = "blue";
+      isColored = true;
+    }
+  });
 })();
