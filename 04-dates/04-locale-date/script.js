0ea1b5ee5020edef9d022c24560ae6f4b6349660
“[Nicol <[nicol.saha@gmail.com]>
2020-07-14 16:40:10 +0200

    Finish Dates-04/4

--- 04-dates/04-locale-date/script.js
+++ 04-dates/04-locale-date/script.js
@@ -15,4 +15,37 @@
 
     // your code here
 
+    // Time + Date
+
+    let now = new Date();
+    let currentDate = document.querySelector("#current-date");
+
+    let date = now.getDate();
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let year = now.getFullYear();
+
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[now.getDay()];
+
+    let months = [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec",
+    ];
+
+    let month = months[now.getMonth()];
+
+    document.getElementById("target").innerHTML = `${day}, ${date} ${month}, ${year}, ${hours}h${minutes} `;
+
+
 })();
