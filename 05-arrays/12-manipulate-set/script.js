bb870c4fdb7f7a7915a6813aab6f9e1d82de54ca
“[Nicol <[nicol.saha@gmail.com]>
2020-07-19 15:48:34 +0200

    Finish Arrays-05/12

--- 05-arrays/12-manipulate-set/script.js
+++ 05-arrays/12-manipulate-set/script.js
@@ -10,18 +10,26 @@
 // You will have time to focus on it later.
 
 (() => {
-    const fruits = new Set([
-        "apple",
-        "pear",
-        "strawberry",
-        "tomato",
-        "orange",
-        "mandarin",
-        "durian",
-        "peach",
-        "grape",
-        "cherry",
-    ]);
+  const fruits = new Set([
+    "apple",
+    "pear",
+    "strawberry",
+    "tomato",
+    "orange",
+    "mandarin",
+    "durian",
+    "peach",
+    "grape",
+    "cherry",
+  ]);
 
-    // your code here
+  // your code here
+
+  document.getElementById("run").addEventListener("click", function () {
+    fruits.delete("cherry");
+    fruits.delete("apple");
+    fruits.add("banana");
+    fruits.add("kiwi");
+    console.log(fruits);
+  });
 })();
