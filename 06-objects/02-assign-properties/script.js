4c645f87b9a7d01541382ea61b964ef2215af05c
“[Nicol <[nicol.saha@gmail.com]>
2020-07-19 19:47:55 +0200

    Finish Objects-06/2

--- 06-objects/02-assign-properties/script.js
+++ 06-objects/02-assign-properties/script.js
@@ -10,24 +10,39 @@
 // You will have time to focus on it later.
 
 (() => {
-  const computers = [
-    { id: "0001", available: false, user: "leny", os: "macOS" },
-    { id: "0002", available: false, user: "Nicolas" },
-    { id: "0003" },
-    { id: "0004", os: "Windows" },
-    { id: "0005" },
-    { id: "0006", os: "macOS" },
-    { id: "0007" },
-    { id: "0008" },
-    { id: "0009", available: false, user: "Anthony" },
-  ];
-  const defaultProps = {
-    available: true,
-    os: "linux",
-    user: null,
-  };
-  // your code here
+  document.getElementById("run").addEventListener("click", function () {
+    const computers = [
+      { id: "0001", available: false, user: "leny", os: "macOS" },
+      { id: "0002", available: false, user: "Nicolas" },
+      { id: "0003" },
+      { id: "0004", os: "Windows" },
+      { id: "0005" },
+      { id: "0006", os: "macOS" },
+      { id: "0007" },
+      { id: "0008" },
+      { id: "0009", available: false, user: "Anthony" },
+    ];
+    const defaultProps = {
+      available: true,
+      os: "linux",
+      user: null,
+    };
+    // your code here
 
-  const newObj = { ...computers, ...defaultProps };
-  console.log(newObj);
+    for (i = 0; i < computers.length; i++) {
+      if (computers[i].available == undefined) {
+        computers[i].available = defaultProps.available;
+      }
+      if (computers[i].user == undefined) {
+        computers[i].user = defaultProps.user;
+      }
+      if (computers[i].os == undefined) {
+        computers[i].os = defaultProps.os;
+      }
+      console.log(computers[i]);
+    }
+
+    // const obj = Object.assign({}, defaultProps, computers[i]);
+    // console.log(obj);
+  });
 })();
