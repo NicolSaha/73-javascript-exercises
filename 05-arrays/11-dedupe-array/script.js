7c2c5dada43fcb9d8d33c3686ae66dbe89025739
“[Nicol <[nicol.saha@gmail.com]>
2020-07-19 15:39:44 +0200

    Finish Arrays-05/11

--- 05-arrays/11-dedupe-array/script.js
+++ 05-arrays/11-dedupe-array/script.js
@@ -9,25 +9,27 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
+  const fruits = [
+    "cerise",
+    "durian",
+    "pomme",
+    "poire",
+    "fraise",
+    "tomate",
+    "orange",
+    "mandarine",
+    "fraise",
+    "durian",
+    "pêche",
+    "cerise",
+    "raisin",
+    "cerise",
+  ];
 
-    const fruits = [
-        "cerise",
-        "durian",
-        "pomme",
-        "poire",
-        "fraise",
-        "tomate",
-        "orange",
-        "mandarine",
-        "fraise",
-        "durian",
-        "pêche",
-        "cerise",
-        "raisin",
-        "cerise",
-    ];
-
-    // your code here
-
+  // your code here
+  document.getElementById("run").addEventListener("click", function () {
+    const fruitsCopy = fruits.map((x) => x);
+    console.log(fruitsCopy);
+  });
 })();
