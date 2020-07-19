899f0ab3d658a04e49bc80491b2a5fa5533bc512
“[Nicol <[nicol.saha@gmail.com]>
2020-07-19 16:40:53 +0200

    Finish Arrays-05/14

--- 05-arrays/14-bird-names-generator/script.js
+++ 05-arrays/14-bird-names-generator/script.js
@@ -10,33 +10,56 @@
 // You will have time to focus on it later.
 
 (() => {
+  document.getElementById("run").addEventListener("click", function () {
     const birds = [
-        {name: "mouette", fem: true},
-        {name: "corbeau"},
-        {name: "mésange", fem: true},
-        {name: "hibou"},
-        {name: "buse", fem: true},
-        {name: "pigeon"},
-        {name: "pie", fem: true},
-        {name: "vautour"},
-        {name: "faucon"},
-        {name: "rouge-gorge"},
-        {name: "tourterelle", fem: true},
-        {name: "corneille", fem: true},
+      { name: "mouette", fem: true },
+      { name: "corbeau" },
+      { name: "mésange", fem: true },
+      { name: "hibou" },
+      { name: "buse", fem: true },
+      { name: "pigeon" },
+      { name: "pie", fem: true },
+      { name: "vautour" },
+      { name: "faucon" },
+      { name: "rouge-gorge" },
+      { name: "tourterelle", fem: true },
+      { name: "corneille", fem: true },
     ];
     const adjectives = new Set([
-        "cendré",
-        "huppé",
-        "chantant",
-        "hurlant",
-        "perché",
-        "grand",
-        "petit",
-        "bleu",
-        "pantelant",
-        "tangent",
-        "arboré",
+      "cendré",
+      "huppé",
+      "chantant",
+      "hurlant",
+      "perché",
+      "grand",
+      "petit",
+      "bleu",
+      "pantelant",
+      "tangent",
+      "arboré",
     ]);
 
     // your code here
+
+    let randomBird = birds[Math.floor(Math.random() * birds.length)];
+    // console.log(randomBird.name);
+
+    let arrAdj = Array.from(adjectives);
+    let randomAdj = arrAdj[Math.floor(Math.random() * arrAdj.length)];
+    // console.log(randomAdj);
+
+    function e() {
+      let eV;
+      if (randomBird.fem === true) {
+        return "e";
+      } else {
+        return " ";
+      }
+    }
+    // console.log(e);
+
+    document.getElementById("target").textContent = `${
+      randomBird.name
+    }${e()} ${randomAdj}`;
+  });
 })();
