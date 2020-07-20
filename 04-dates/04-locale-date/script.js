5a7a9ffeb980b951a658bdfedfe00eabb388f215
“[Nicol <[nicol.saha@gmail.com]>
2020-07-20 16:34:06 +0200

    Finish  Dates-04/4

--- 04-dates/04-locale-date/script.js
+++ 04-dates/04-locale-date/script.js
@@ -9,45 +9,49 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
+  // to change the content of a tag: document.getElementById("element-id").innerHTML = "new-value"
 
-    // to change the content of a tag: document.getElementById("element-id").innerHTML = "new-value"
+  // your code here
 
-    // your code here
+  // Time + Date
 
-    // Time + Date
+  let now = new Date();
 
-    let now = new Date();
+  let date = now.getDate();
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  let year = now.getFullYear();
 
-    let date = now.getDate();
-    let hours = now.getHours();
-    let minutes = now.getMinutes();
-    let year = now.getFullYear();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[now.getDay()];
 
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    let day = days[now.getDay()];
+  let months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
 
-    let months = [
-        "Jan",
-        "Feb",
-        "Mar",
-        "Apr",
-        "May",
-        "Jun",
-        "Jul",
-        "Aug",
-        "Sep",
-        "Oct",
-        "Nov",
-        "Dec",
-    ];
+  let month = months[now.getMonth()];
 
-    let month = months[now.getMonth()];
+  let result = `${day}, ${date} ${month}, ${year}, ${hours}h${minutes}`;
 
-    let result = `${day}, ${date} ${month}, ${year}, ${hours}h${minutes}`;
-   
-
-    document.getElementById("target").innerHTML= result;
-
-
+  document.getElementById("target").innerHTML = result;
 })();
