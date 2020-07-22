53dd604d0637771c71e0bf7aa04a528ce1430b04
“[Nicol <[nicol.saha@gmail.com]>
2020-07-22 10:09:56 +0200

    Finish Misc-09/2

--- 09-misc/02-typewriter-effect/script.js
+++ 09-misc/02-typewriter-effect/script.js
@@ -9,8 +9,31 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
+  // your code here
+  let tw = {
+    /* [THE SETTINGS] */
+    container: "target", // ID of typewriter container
+    text:
+      "Je suis un texte qui va apparaître dans un effet 'machine à écrire', un peu comme dans les films noirs. Sauf que... je ne sais pas qui est l'assassin ! Y en a-t-il vraiment un ? Habite-t-il au 21 ? Que de mystères !", // Text to show
+    delay: 100, // Delay in between each character
 
-    // your code here
+    /* [THE MECHANICS] */
+    timer: null, // Used to hold the timer
+    pointer: 0, // Current text position
+    draw: function () {
+      // tw.draw() : typewriter effect
 
+      tw.pointer++;
+      tw.container.innerHTML = tw.text.substring(0, tw.pointer);
+      if (tw.pointer < tw.text.length) {
+        tw.timer = setTimeout(tw.draw, tw.delay);
+      }
+    },
+  };
+
+  window.addEventListener("load", function () {
+    tw.container = document.getElementById(tw.container);
+    tw.draw();
+  });
 })();
