5b7c11713508e0b44fb9f95bec1ed110d59e886f
“[Nicol <[nicol.saha@gmail.com]>
2020-07-24 00:21:54 +0200

    Finish DOM-08/12

--- 08-dom/12-change-event-input-two/script.js
+++ 08-dom/12-change-event-input-two/script.js
@@ -9,8 +9,26 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
+  // your code here
+  const passOne = document.querySelector("#pass-one");
+  const validity = document.querySelector("#validity");
 
-    // your code here
+  passOne.onkeyup = () => {
+    const valueOne = passOne.value;
+    const length = valueOne.length;
 
+    let n = 0;
+    for (let i = 0; i < valueOne.length; i++) {
+      if (!isNaN(valueOne[i])) {
+        n++;
+      }
+    }
+
+    if (length >= 8 && n >= 2) {
+      validity.textContent = `ok`;
+    } else {
+      validity.textContent = `not ok`;
+    }
+  };
 })();
