de899796f0ad302cf1f7e8020415c9edb3d2d3e7
“[Nicol <[nicol.saha@gmail.com]>
2020-07-23 22:14:22 +0200

    Finish Fetch-11/2

--- 11-fetch/02-list-to-template/script.js
+++ 11-fetch/02-list-to-template/script.js
@@ -10,5 +10,38 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+  // your code here
+  document.getElementById("run").addEventListener("click", function () {
+    let myInit = {
+      method: "GET",
+      mode: "cors",
+      cache: "default",
+    };
+
+    let myRequest = new Request(
+      "/Exercises/73-javascript-exercises/shared/api.json",
+      myInit
+    );
+
+    fetch(myRequest)
+      .then((res) => res.json())
+      .then((data) => {
+        console.table(Object.values(data.heroes));
+        const arr = Object.values(data.heroes);
+        //return arr;
+
+        const template = document.getElementById("tpl-hero");
+        for (i = 0; i < arr.length; i++) {
+          const hero = arr[i];
+          const clone = template.content.cloneNode(true);
+          const name = clone.querySelectorAll(".name");
+          const alterego = clone.querySelectorAll(".alter-ego");
+          const powers = clone.querySelectorAll(".powers");
+          name[0].innerHTML = hero.name;
+          alterego[0].innerHTML = hero.alterEgo;
+          powers[0].innerHTML = hero.abilities;
+          template.parentNode.appendChild(clone);
+        }
+      });
+  });
 })();
