c3c116c1d41e3e4e005ee3de2ea82c847afeb2d0
“[Nicol <[nicol.saha@gmail.com]>
2020-07-24 00:14:00 +0200

    Finish DOM-08/11

--- 08-dom/11-change-event-input-one/script.js
+++ 08-dom/11-change-event-input-one/script.js
@@ -9,8 +9,20 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
+  // your code here
+  const passOne = document.querySelector("#pass-one");
+  const counterSpan = document.querySelector("#counter");
 
-    // your code here
+  passOne.onkeyup = () => {
+    let valueOne = passOne.value;
+    const length = valueOne.length;
+    counterSpan.textContent = `${length}/10`;
 
+    if (length > 10) {
+      valueOne = valueOne.slice(0, -1);
+      passOne.value = valueOne;
+      counterSpan.textContent = `10/10`;
+    }
+  };
 })();
