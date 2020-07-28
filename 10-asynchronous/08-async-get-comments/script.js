28528f0186cd7fed2e83c8169de7b3a544570b4c
“[Nicol <[nicol.saha@gmail.com]>
2020-07-28 10:21:08 +0200

    Finish Asynch-10/8

--- 10-asynchronous/08-async-get-comments/script.js
+++ 10-asynchronous/08-async-get-comments/script.js
@@ -10,5 +10,24 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+  // your code here
+  document.querySelector("#run").onclick = () => {
+    window.lib.getPosts().then(getPosts).catch(error);
+  };
+
+  async function getPosts(articles) {
+    await articles;
+    for (const obj in articles) {
+      const id = articles[obj].id;
+      window.lib
+        .getComments(id)
+        .then((comments) => (articles[obj].comments = comments))
+        .catch(error);
+    }
+    console.log(articles);
+  }
+  async function error(error) {
+    await error;
+    console.error(error);
+  }
 })();
