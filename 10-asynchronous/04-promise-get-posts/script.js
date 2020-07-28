bd911bf66cc7e2f82adc7c8496e88d03f7e1eddc
“[Nicol <[nicol.saha@gmail.com]>
2020-07-28 10:14:10 +0200

    Finish Asynch-10/4

--- 10-asynchronous/04-promise-get-posts/script.js
+++ 10-asynchronous/04-promise-get-posts/script.js
@@ -10,5 +10,9 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+  // your code here
+  document.querySelector("#run").onclick = () => {
+    window.lib.getPosts().then((articles) => console.log(articles));
+    //.catch((error) => console.log(error));
+  };
 })();
